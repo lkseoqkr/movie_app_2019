a7dc9476d335936430d3aeb39110cbd1f336dbb1
kyungsoo.lee <lkseoqkr@gmail.com>
2020-06-24 22:20:37 +0900

    Updates

---
+++ precache-manifest.1e2cf059cc31eded4b082e64882955b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be5439d34849eb3fce804bd031ec0cf",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "09fbcd0de57ce43b4c8e",
+    "url": "/movie_app_2019/static/css/main.98e12858.chunk.css"
+  },
+  {
+    "revision": "f830e794604987d4bdc4",
+    "url": "/movie_app_2019/static/js/2.e5efe953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.e5efe953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fbcd0de57ce43b4c8e",
+    "url": "/movie_app_2019/static/js/main.caeacb96.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
